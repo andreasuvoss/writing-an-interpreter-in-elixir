1407ec631c7cb13daa13c9b73d0203da7e9bf204
Andreas Voss <andreas@anvo.dk>
2025-10-11 13:45:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Writing An Interpreter In Elixir
+
+This repo will contain the result of working through the book 'Writing an Interpreter in Go' by Thorsten Ball. I will
+just attempt to implement everything in Elixir instead.
+
+## TODO
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Flake for this project?";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.elixir
+            pkgs.watchman
+            pkgs.inotify-tools
+          ];
+        };
+      }
+    );
+}
